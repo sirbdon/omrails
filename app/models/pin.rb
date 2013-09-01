32c05caaf5630dc414227796a7564639b55ff161
Brandon Pizzacalla <bpizzacalla@gmail.com>
2013-09-01 11:23:19 -0400

    add faker gem

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -12,8 +12,7 @@ class Pin < ActiveRecord::Base
   belongs_to :user
 
   def image_remote_url=(url_value)
-  	self.image = URI.parse(url_value) unless url_value.blank?
-  	super
-  end	
-  
+    self.image = URI.parse(url_value) unless url_value.blank?
+    super
+  end
 end
